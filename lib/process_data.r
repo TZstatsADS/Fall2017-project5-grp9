9ff376e13fe1593d7cb8f0d3048c34584beaa165
Saaya Yasuda <saaya.yasuda@gmail.com>
2017-12-03 20:18:21 -0500

    writing process_data.r (WIP)

--- lib/process_data.r
+++ lib/process_data.r
@@ -18,28 +18,46 @@ if(length(packages.needed)>0){
 library(tuneR)
 
 #######################################
-# load data
+# load & clean csv files
 #######################################
 
-other_dev = read.csv("./data/cv-other-dev.csv",header=T,na.strings=c("","NA"))
-other_train = read.csv("./data/cv-other-train.csv",header=T,na.strings=c("","NA"))
-valid_test = read.csv("./data/cv-valid-test.csv",header=T,na.strings=c("","NA"))
+other_dev = read.csv("./data/cv-other-dev.csv",header=T,
+                     na.strings=c("","NA"),
+                     colClasses=c("filename"="character","text"="character"))
+other_train = read.csv("./data/cv-other-train.csv",header=T,
+                     na.strings=c("","NA"),
+                     colClasses=c("filename"="character","text"="character"))
+valid_test = read.csv("./data/cv-valid-test.csv",header=T,
+                     na.strings=c("","NA"),
+                     colClasses=c("filename"="character","text"="character"))
 
 # remove NA rows (i.e. files without labels)
 other_dev = other_dev[,-ncol(other_dev)]
 other_dev = na.omit(other_dev)
+row.names(other_dev) = 1:nrow(other_dev)
 
 other_train = other_train[,-ncol(other_train)]
 other_train = na.omit(other_train)
+row.names(other_train) = 1:nrow(other_train)
 
 valid_test = valid_test[,-ncol(valid_test)]
 valid_test = na.omit(valid_test)
+row.names(valid_test) = 1:nrow(valid_test)
 
 # Saving them as csv
 write.csv(other_dev,file="./output/other_dev_clean.csv",row.names=FALSE)
 write.csv(other_train,file="./output/other_train_clean.csv",row.names=FALSE)
 write.csv(valid_test,file="./output/valid_test_clean.csv",row.names=FALSE)
 
+# summary(other_dev)
 
+#######################################
+# load data based on csv
+#######################################
+
+#test
+filename = other_dev[1,1]
+w = readMP3(paste0("./data/",filename))
+melw = melfcc(w)
 
 
