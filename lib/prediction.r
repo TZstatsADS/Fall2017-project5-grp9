33bfa7adffba1016a469992055abefc8cdb2fe78
Saaya Yasuda <saaya.yasuda@gmail.com>
2017-12-06 06:18:14 -0500

    XGBoost & logistic. Added predictions to main.Rmd

--- lib/prediction.r
+++ lib/prediction.r
@@ -6,7 +6,7 @@
 rm(list=ls())
 setwd('~/Documents/Github/fall2017-project5-proj5-grp9')
 
-packages.used=c("nnet","gbm","caret","randomForest","e1071","xgboost")
+packages.used=c("nnet","gbm","caret","randomForest","e1071","xgboost","dplyr")
 
 packages.needed=setdiff(packages.used, intersect(installed.packages()[,1], packages.used))
 if(length(packages.needed)>0){
@@ -18,6 +18,7 @@ library(randomForest) #RF
 library(e1071) #SVM etc
 library(gbm) #gbm
 library(xgboost)
+library(dplyr)
 
 RUNALL = FALSE # If this is true, it runs time consuming functions.
 
@@ -49,7 +50,8 @@ uncommon =data[data$country %in% uncommon,]
 common =data[data$country %in% common,]
 uncommon$country="other"
 data = rbind(common,uncommon)
-data$country = droplevels(data$country)
+
+data$country = droplevels(data$country) # reduce levels
 
 
 #######################################
@@ -80,10 +82,10 @@ MSE_svm #185.0999
 
 #tuning - takes REALLY LONG TIME to run. didn't finish.
 if (RUNALL){
-svmtuned_age <- tune(svm, age ~ .,  data = train_age, 
-                     ranges = list(epsilon = seq(0,1,0.1), cost = 2^(2:9)))
-print(svmtuned_age)
-#plot(svmtuned_age)
+  svmtuned_age <- tune(svm, age ~ .,  data = train_age, 
+                       ranges = list(epsilon = seq(0,1,0.1), cost = 2^(2:9)))
+  print(svmtuned_age)
+  #plot(svmtuned_age)
 }
 #svmfit_age_better = svm(age ~ ., train_age)
 #svmpred_age_better = predict(svmfit_age_better, test_age)
@@ -107,16 +109,16 @@ postResample(svmpred_sex, test_sex$sex)
 
 #tuning - takes a while to run
 if (RUNALL){
-svmtuned_sex <- tune(svm, sex ~ .,  data = train_sex, 
-                     ranges = list(epsilon = seq(0,1,0.1), cost = 2^(2:9)))
-print(svmtuned_sex)
-#- best parameters:
-#  epsilon cost
-#0    8
-#png(filename="./output/svm_tuned_sex.png")
-#plot(svmtuned_sex)
-#dev.off()
-
+  svmtuned_sex <- tune(svm, sex ~ .,  data = train_sex, 
+                       ranges = list(epsilon = seq(0,1,0.1), cost = 2^(2:9)))
+  print(svmtuned_sex)
+  #- best parameters:
+  #  epsilon cost
+  #0    8
+  #png(filename="./output/svm_tuned_sex.png")
+  #plot(svmtuned_sex)
+  #dev.off()
+  
 }
 svmfit_sex_better = svm(sex ~ ., train_sex, epsilon=0, cost=8)
 svmpred_sex_better = predict(svmfit_sex_better, test_sex)
@@ -242,90 +244,192 @@ which.max(accuracy_vec_country)
 
 
 #######################################
-# XGBoost WIP
+# XGBoost
 #######################################
 
+set.seed(123)
+params_df = expand.grid(nrounds=c(100,200),
+                        eta = c(0.1,0.3,0.5),
+                        gamma=1,
+                        max_depth = c(3,5,7,10),
+                        colsample_bytree=c(0.5,0.7,0.9),
+                        min_child_weight=1:2,
+                        subsample = c(0.5,0.75,1))
 
-
-xgb.cv(data = train_sex,nrounds=5, nfold=5,
-       objective="multi:softprob",eval_metric="mlogloss")
-
-
-basic = xgboost(data = train_matrix,
-                max.depth=3,eta=0.01,nthread=2,nround=50,
-                objective = "multi:softprob",
-                eval_metric = "mlogloss",
-                num_class = 3,
-                verbose = F)
-
-
+train_control = trainControl(method = "cv", number = 5,
+                             verboseIter = T, returnData = F,
+                             returnResamp = "all", allowParallel = T)
 
 
 
-multinom_fit = multinom(formula = as.factor(country) ~ .,
+# Sex
+labels_train = as.matrix(as.integer(train_sex$sex)-1)
+xgb_sex_cv = xgb.cv(data =subset(data.matrix(train_sex), select=-sex),
+                    test = data.matrix(test_sex),
+                    label = labels_train, nrounds=10, nfold=5, num_class=2,nthread=2,
+                    objective="multi:softmax",eval_metric="merror",prediction=T)
+
+xgb_train_sex = train(x=subset(data.matrix(train_sex), select=-sex), 
+                      y=as.factor(labels_train),
+                      trControl = train_control,
+                      tuneGrid = params_df,
+                      method = "xgbTree")
+#best param
+head(xgb_train_sex$results[with(xgb_train_sex$results,order(Accuracy, decreasing=T)),],5)
+xgb_train_sex$bestTune
+#     nrounds max_depth eta gamma colsample_bytree min_child_weight subsample
+#26     200         3 0.1     1              0.9                1       0.5
+
+xgbfit_sex = xgboost(data =subset(data.matrix(train_sex), select=-sex),
+                     label = labels_train, objective="multi:softmax",
+                     eval_metric="merror",num_class=2, verbose=F,
+                     params = xgb_train_sex$bestTune,
+                     nrounds=xgb_train_sex$bestTune$nrounds)
+
+labels_test = as.matrix(as.integer(test_sex$sex)-1)
+test_sex2 = data.frame(labels_test, subset(data.matrix(test_sex), select=-sex))
+
+xgpred_sex = predict(xgbfit_sex, subset(data.matrix(test_sex), select=-sex),reshape=T)
+xgpred_sex = factor(xgpred_sex,labels = c("female","male"))
+
+postResample(xgpred_sex, test_sex$sex)
+#Accuracy     Kappa 
+#0.9421875 0.8843682 
+table(xgpred_sex, test_sex$sex)
+#xgpred_sex female male
+#female    299   19
+#male       18  304
+
+
+
+# Re-set up params for the two dfs that take a long time to run on.
+params_df2 = expand.grid(nrounds=c(100),
+                        eta = c(0.1,0.3,0.5),
+                        gamma=1,
+                        max_depth = c(3,5,7),
+                        colsample_bytree=c(0.5,0.7,0.9),
+                        min_child_weight=1:2,
+                        subsample = c(0.5,1))
+
+train_control2 = trainControl(method = "cv", number = 3,
+                             verboseIter = T, returnData = F,
+                             returnResamp = "all", allowParallel = T)
+
+
+
+# Country
+set.seed(123)
+labels_train = as.matrix(as.integer(train_country$country)-1)
+
+xgb_country_cv = xgb.cv(data =subset(data.matrix(train_country), select=-country),
+                        test = data.matrix(test_country),
+                        label = labels_train, nrounds=10, nfold=5, num_class=88,nthread=2,
+                        objective="multi:softmax",eval_metric="merror",prediction=T)
+
+xgb_train_country = train(x=subset(data.matrix(train_country), select=-country), 
+                          y=as.factor(labels_train),
+                          trControl = train_control2,
+                          tuneGrid = params_df2,
+                          method = "xgbTree")
+#best param
+head(xgb_train_country$results[with(xgb_train_country$results,order(Accuracy, decreasing=T)),],5)
+xgb_train_country$bestTune
+#     nrounds max_depth eta gamma colsample_bytree min_child_weight subsample
+#28     200         3 0.1     1              0.9                1      0.75
+
+xgbfit_country = xgboost(data =subset(data.matrix(train_country), select=-country),
+                         label = labels_train, objective="multi:softprob",
+                         eval_metric="merror",num_class=88, verbose=F,
+                         params = xgb_train_country$bestTune,
+                         nrounds=xgb_train_country$bestTune$nrounds)
+
+labels_test = as.matrix(as.integer(test_country$country)-1)
+
+xgpred_country = predict(xgbfit_country, 
+                         subset(data.matrix(test_country), select=-country),reshape=T)
+colnames(xgpred_country) = levels(test_country$country)
+xgpred_country = cbind(xgpred_country, maxcol=apply(xgpred_country, 1, which.max))
+
+which.max(xgpred_country[100,])
+
+xgpred_country = data.frame(xgpred_country) %>%
+  mutate(max_prob=which.max(.))
+#mutate(max_prob=max.col(.,"last"))
+
+xgpred_country = factor(xgpred_country,labels = levels(test_country$country))
+
+postResample(xgpred_country, test_country$country)
+table(xgpred_country, test_country$country)
+
+
+# Age
+set.seed(123)
+labels_train = as.matrix(as.integer(train_age$age)-1)
+
+xgb_age_cv = xgb.cv(data =subset(data.matrix(train_age), select=-age),
+                    test = data.matrix(test_age),
+                    label = labels_train, nrounds=10, nfold=5, num_class=88,nthread=2,
+                    objective="multi:softmax",eval_metric="merror",prediction=T)
+
+xgb_train_age = train(x=subset(data.matrix(train_age), select=-age), 
+                      y=as.factor(labels_train),
+                      trControl = train_control2,
+                      tuneGrid = params_df2,
+                      method = "xgbTree")
+#best param
+head(xgb_train_age$results[with(xgb_train_age$results,order(Accuracy, decreasing=T)),],5)
+xgb_train_age$bestTune
+#     nrounds max_depth eta gamma colsample_bytree min_child_weight subsample
+#28     200         3 0.1     1              0.9                1      0.75
+
+xgbfit_age = xgboost(data =subset(data.matrix(train_age), select=-age),
+                     label = labels_train, objective="multi:softprob",
+                     eval_metric="merror",num_class=88, verbose=F,
+                     params = xgb_train_age$bestTune,
+                     nrounds=xgb_train_age$bestTune$nrounds)
+
+labels_test = as.matrix(as.integer(test_age$age)-1)
+
+xgpred_age = predict(xgbfit_age, 
+                     subset(data.matrix(test_age), select=-age),reshape=T)
+colnames(xgpred_age) = levels(test_age$age)
+xgpred_age = cbind(xgpred_age, maxcol=apply(xgpred_age, 1, which.max))
+
+
+
+
+
+
+
+
+#######################################
+# Logistic regression
+#######################################
+
+# Sex
+set.seed(123)
+lgfit_sex = glm(formula = sex ~ ., data=train_sex, family=binomial(link='logit'))
+lgpred_sex = predict(lgfit_sex, test_sex)
+table(lgpred_sex, test_sex$sex)
+postResample(lgpred_sex, test_sex$sex)
+
+# Age
+set.seed(123)
+lgfit_age = glm(formula = age ~ ., data=train_age, family=binomial(link='logit'))
+lgpred_age = predict(lgfit_age, test_age)
+MSE_lg = mean( (lgpred_age - test_age$age)^2)
+MSE_lg #
+
+#Country
+set.seed(123)
+lgfit_country = multinom(formula = country ~ .,
                         data=train, MaxNWts = 100000, maxit = 100)
-pred = predict(multinom_fit, type="class", newdata=test)
-postResample(pred, test$country)
-confusionMatrix(pred, test$country)$table
-
-multinom_fit2 = multinom(formula = as.factor(sex) ~ .,
-                        data=train, MaxNWts = 100000, maxit = 100)
-pred2 = predict(multinom_fit2, newdata=test, type="class")
-postResample(pred2, test$sex)
-
-multinom_fit2 = multinom(formula = as.factor(sex) ~ .,
-                         data=train, MaxNWts = 100000, maxit = 100)
-pred2 = predict(multinom_fit2, newdata=test, type="class")
-postResample(pred2, test$sex)
-
-multinom_fit3 = multinom(formula = age ~ .,
-                         data=train, MaxNWts = 200000, maxit = 100)
-pred2 = predict(multinom_fit2, newdata=test, type="class")
-postResample(pred2, test$sex)
-
-
-logistic_fit2
+lgpred_country = predict(lgfit_country, test_country)
+#table(lgpred_country, test_country$country)
+postResample(lgpred_country, test_country$country)
 
 
 
-test_result(test_country,multinom_fit$fitted.values,T)
-
-
-multinom_train = function(train_data, y, is_factor){
-  set.seed(123)
-  if(is_factor){
-    multinom_fit = multinom(formula = as.factor(y) ~ .,
-                            data=train_data, MaxNWts = 100000, maxit = 500)
-  }
-  else{
-    multinom_fit = multinom(formula = unlist(y) ~ .,
-                            data=train_data, MaxNWts = 100000, maxit = 500)
-  }
-  return(fit=multinom_fit)
-}
-
-### Run it:
-# Training - age
-age_multinom_fit = multinom_train(train_age, train_age["age"], is_factor=FALSE)
-
-country_multinom_fit = multinom_train(train_country, train_country["country"], is_factor=T)
-# Testing - age
-test_result(test_age, age_multinom_fit, is_factor=TRUE)
-
-# the rest
-RUN = FALSE
-if (RUN){
-  gender_multinom_fit = multinom_train(gender_train, gender_train[,1])
-  accent_multinom_fit = multinom_train(accent_train, accent_train[,1])
-  test_result(gender_test, gender_multinom_fit)
-  test_result(accent_test, accent_multinom_fit)
-}
-
-# 161.653 secs
-#system.time(multinom_train(age_train, age_train[,1]))
-
-# VERY time consuming.
-#stepwisefit = step(age_multinom_fit$fit, direction="both",scope=formula(age_multinom_fit$fit))
 
 
 #######################################
